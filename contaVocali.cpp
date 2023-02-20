cc8571b4e0d78ea8e2f4b1cb4f1823225ff60695
Marco Boccia <pokermark95@live.it>
2023-02-20 12:05:13 +0100

    finito esercizio contaVocali

---
+++ contaVocali.cpp
@@ -0,0 +1,26 @@
+// Scrivi un programma in C++ che prenda in input una stringa di caratteri e ne conti il numero di vocali utilizzando.
+
+#include <iostream>
+using namespace std;
+
+// solution 1
+
+int main()
+{
+    string parola;
+    int contatoreVocali = 0;
+    cout << "Inserisci una parola" << endl;
+    cin >> parola;
+
+    for (int i = 0; i < parola.length(); i++)
+    {
+        char carattereInI = parola[i];
+        if (carattereInI == 'a' || carattereInI == 'e' || carattereInI == 'i' || carattereInI == 'o' || carattereInI == 'u' ||
+            carattereInI == 'A' || carattereInI == 'E' || carattereInI == 'I' || carattereInI == 'O' || carattereInI == 'U')
+        {
+            contatoreVocali++;
+        }
+    }
+        cout << "Il numero di vocali nella stringa inserita è: " << contatoreVocali << endl;
+    return 0;
+}
